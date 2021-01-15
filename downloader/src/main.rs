d52e16a5bdc5c7aa9f039a1b4275cbf6ef9aebef
Berke DURAK <bd@exhrd.fr>
2021-01-15 12:17:13 +0100

    Query server and extract footprints

--- downloader/src/main.rs
+++ downloader/src/main.rs
@@ -3,6 +3,7 @@ mod misc_error;
 use misc_error::MiscError;
 use std::error::Error;
 use url::Url;
+use xml::reader::{EventReader,XmlEvent};
 
 #[tokio::main]
 async fn main()->Result<(),Box<dyn Error>> {
@@ -16,5 +17,61 @@ async fn main()->Result<(),Box<dyn Error>> {
 	.text()
 	.await?;
     println!("RESP: {:?}",resp);
+    let mut ev = EventReader::from_str(&resp);
+    #[derive(Copy,Clone)]
+    enum State { Init,Footprint,Done }
+    let mut q = State::Init;
+    let mut footprints = Vec::new();
+    loop {
+	match ev.next() {
+	    Ok(e) => {
+		println!("EV {:?}",e);
+		match (q,e) {
+		    (State::Init,XmlEvent::StartElement{ name, attributes, namespace }) => { // if name == "gmlfootprint" => {
+			if name.local_name == "str" {
+			    if let Some(pf) = namespace.get("opensearch") {
+				// println!("OSEARCH PF {}",pf);
+				if let Some(a) = attributes.iter().find(|&a| a.name.local_name == "name") {
+				    if a.value == "gmlfootprint" {
+					q = State::Footprint;
+				    }
+				    // println!("name={}",a.value);
+				    // 		       // 	       println!("LOCNAME {} VAL {}",
+				    // 		       // 			a.name.local_name, a.value);
+				    // 		       // 	   }
+				    // 		       // 	   false
+				    // 		       // });
+				}
+			    }
+			}
+			// println!("NAME {:?}",name);
+			// println!("ATTR {:?}",attributes);
+			// println!("NAMESPACE {:?}",namespace);
+		    },
+		    (State::Footprint,XmlEvent::Characters(u)) => {
+			// println!("GML: {}",u);
+			footprints.push(u);
+			q = State::Init;
+		    }
+		    (_,XmlEvent::EndElement{ name, .. }) => {
+			// println!("END {}",name);
+		    },
+		    (_,XmlEvent::EndDocument) => break,
+		    (_,x) => {
+			// println!("OTHER {:?}",x);
+		    },
+		}
+	    },
+	    Err(e) => {
+		println!("ERR: {}",e);
+		break;
+	    }
+	}
+    }
+    let n_footprint = footprints.len();
+    println!("Number of footprints found: {}",n_footprint);
+    for f in footprints.iter() {
+	println!(">>> {}",f);
+    }
     Ok(())
 }
