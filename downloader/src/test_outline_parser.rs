7198e545c28ba22518a61a0fccee450ccc30fed0
Berke DURAK <bd@exhrd.fr>
2021-01-15 17:26:07 +0100

    First version of "MULTIPOLY" parser.

---
+++ downloader/src/test_outline_parser.rs
@@ -0,0 +1,15 @@
+mod outline_parser;
+
+pub fn main() {
+    let u = std::env::args().nth(1).expect("No string to parse");
+    println!("INPUT: {:?}",u);
+    match outline_parser::multipolygon(&u) {
+	Ok((rest,middle)) => {
+	    println!("OUT: {:?}",middle);
+	    println!("REST: {:?}",rest);
+	},
+	Err(e) => {
+	    panic!("ERROR: {:?}",e);
+	}
+    }
+}
