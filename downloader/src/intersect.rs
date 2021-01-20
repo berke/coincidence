7a1e68d94ecadb9917d0360094aae3423dc523b7
Berke DURAK <bd@exhrd.fr>
2021-01-20 18:06:35 +0100

    Intersection check nominally implemented, but assumed that the first ring of the returned polygons is the outer ring.  To be checked.

--- downloader/src/intersect.rs
+++ downloader/src/intersect.rs
@@ -10,6 +10,35 @@ use std::error::Error;
 use minisvg::MiniSVG;
 use footprint::{Footprint,Footprints};
 
+use geo::{MultiPolygon,Polygon,LineString};
+use geo::algorithm::intersects::Intersects;
+
+fn as_multipolygon(f:&Footprint)->MultiPolygon<f64> {
+    let mut u = Vec::new();
+    // f.outline: Vec<Vec<Vec<(f64,f64)>>>
+    // f.outline.iter(): &Vec<Vec<(f64,f64)>>
+    // f.outline.iter().iter(): &Vec<(f64,f64)>
+    for a in f.outline.iter() {
+	let m = a.len();
+	if m > 0 {
+	    let exterior : LineString<f64> = a[0].clone().into();
+	    let interior : Vec<LineString<f64>> = a.iter().skip(1).map(|o| {
+		let ls : LineString<f64> = o.clone().into();
+		ls
+	    }).collect();
+	    let poly = Polygon::new(exterior,interior);
+	    u.push(poly);
+	}
+    }
+    MultiPolygon::from(u)
+}
+
+fn check_intersection(f1:&Footprint,f2:&Footprint)->bool {
+    let m1 = as_multipolygon(f1);
+    let m2 = as_multipolygon(f2);
+    m1.intersects(&m2)
+}
+
 fn main()->Result<(),Box<dyn Error>> {
     let fp1_fn = MiscError::from_option(std::env::args().nth(1),"Specify path to first footprint file")?;
     let fp2_fn = MiscError::from_option(std::env::args().nth(2),"Specify path to second footprint file")?;
@@ -26,7 +55,12 @@ fn main()->Result<(),Box<dyn Error>> {
 	    let t2 = f2.mean_time();
 	    let delta_t = (t1 - t2).abs();
 	    if delta_t < delta_t_max {
-		println!("Checking {} vs {} (time difference {})",f1.id,f2.id,delta_t);
+		let x = check_intersection(f1,f2);
+		if x {
+		    println!("Intersection: {} vs {} (time difference {})",f1.id,f2.id,delta_t);
+		} else {
+		    println!("No intersection: {} vs {} (time difference {})",f1.id,f2.id,delta_t);
+		}
 	    }
 	}
     }
