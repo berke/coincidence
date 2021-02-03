1fba7699a9d6b279a3de400db1dfebe515024ea8
Berke DURAK <bd@exhrd.fr>
2021-02-03 17:52:47 +0100

    Clean up, and perform antimeridian cutting.

--- downloader/src/iasifpex.rs
+++ downloader/src/iasifpex.rs
@@ -11,11 +11,10 @@ use std::fs::File;
 use std::io::{BufReader,BufRead};
 
 use log::{error,info};
-use chrono::{DateTime,NaiveDate,Duration,Utc};
+use chrono::{DateTime,NaiveDate,Utc};
 use clap::{Arg,App};
 use misc_error::MiscError;
 use footprint::{Footprint,Footprints};
-use ndarray::{ArrayD,Array1,Array2,Array4};
 
 struct IASINexIterator {
     buf:BufReader<File>,
@@ -28,8 +27,7 @@ struct IASINexIterator {
 impl IASINexIterator {
     pub fn new<P:AsRef<Path>>(path:P)->Result<Self,Box<dyn Error>> {
 	let fd = File::open(&path)?;
-	let mut buf = BufReader::new(fd);
-	let mut line = String::new();
+	let buf = BufReader::new(fd);
 
 	let mut pb = PathBuf::new();
 	pb.push(&path);
@@ -106,7 +104,7 @@ fn main()->Result<(),Box<dyn Error>> {
 
     let mut footprints = Vec::new();
 
-    'outer: for nex_fn in nex_fns {
+    for nex_fn in nex_fns {
 	info!("Processing file {}",nex_fn);
 
 	let orbit = 0; // XXX
@@ -119,6 +117,7 @@ fn main()->Result<(),Box<dyn Error>> {
 	let mut scan = Vec::new();
 	let mut igra = 1;
 	let mut done = false;
+	let mut ncross = 0;
 
 	while !done {
 	    let mut flush = None;
@@ -143,20 +142,24 @@ fn main()->Result<(),Box<dyn Error>> {
 		    info!("Granule {}: got {} pixels",igra,npix);
 
 		    let id = format!("{}/{}",dataset_id,igra);
-		    let mut poly = Vec::new();
+		    let mut ring = Vec::new();
 
-		    poly.push(scan[0].outline[3]);
+		    ring.push(scan[0].outline[3]);
 		    for i in 0..npix {
-			poly.push(scan[i].outline[2]);
+			ring.push(scan[i].outline[2]);
 		    }
 
-		    poly.push(scan[npix - 1].outline[1]);
+		    ring.push(scan[npix - 1].outline[1]);
 
 		    for i in (1..npix).rev() {
-			poly.push(scan[i].outline[0]);
+			ring.push(scan[i].outline[0]);
 		    }
 		    
-		    let outline = vec![vec![poly]];
+		    let mut outline = Vec::new();
+		    if amcut::cut_and_push(&mut outline,ring) {
+			ncross += 1;
+		    }
+		    // let outline = vec![vec![poly]];
 
 		    let t0 = scan.iter().fold(scan[0].t,|q,x| q.min(x.t));
 		    let t1 = scan.iter().fold(scan[0].t,|q,x| q.max(x.t));
@@ -178,66 +181,7 @@ fn main()->Result<(),Box<dyn Error>> {
 		}
 	    }
 	}
-
-	// for x in nexs {
-	// }
-
-	// let gr = fd.group("/METADATA/EOP_METADATA/om:procedure/eop:instrument")?;
-	// let instrument : &hdf5::types::FixedAscii<[u8;16]> = &gr.attribute("eop:shortName")?.read_raw()?[0];
-	// info!("Instrument: {}",instrument);
-
-	// let gr = fd.group("/METADATA/EOP_METADATA/om:procedure/eop:platform")?;
-	// let platform : &hdf5::types::FixedAscii<[u8;16]> = &gr.attribute("eop:shortName")?.read_raw()?[0];
-	// info!("Platform: {}",platform);
-
-	// let orbit = fd.attribute("orbit")?.read_raw::<i32>()?[0] as usize;
-	// info!("Orbit: {}",orbit);
-
-	let mut ncross = 0;
-
-	// 	for igra in 0..ngra {
-	// 	    let mut iscan = scan0;
-	// 	    loop {
-	// 		if iscan >= scan0+nscan {
-	// 		    break;
-	// 		}
-	// 		let mut outline : Vec<Vec<Vec<(f64,f64)>>> = Vec::new();
-	// 		let mut ring = Vec::new();
-	// 		for ipix in 0..npix {
-	// 		    ring.push((lons[[igra,iscan,ipix,0]] as f64,
-	// 			       lats[[igra,iscan,ipix,0]] as f64));
-	// 		}
-	// 		ring.push((lons[[igra,iscan,npix - 1,1]] as f64,
-	// 			   lats[[igra,iscan,npix - 1,1]] as f64));
-	// 		for ipix in (0..npix).rev() {
-	// 		    ring.push((lons[[igra,iscan,ipix,2]] as f64,
-	// 			       lats[[igra,iscan,ipix,2]] as f64));
-	// 		}
-	// 		ring.push((lons[[igra,iscan,0,3]] as f64,
-	// 			   lats[[igra,iscan,0,3]] as f64));
-
-	// 		if amcut::cut_and_push(&mut outline,ring) {
-	// 		    ncross += 1;
-	// 		}
-	
-	// 		let t_obs = tropomi_t0 + Duration::seconds(times[[igra]] as i64) + Duration::milliseconds(delta_times[[igra,iscan]] as i64);
-	// 		let t0 = t_obs.timestamp_millis() as f64 / 1000.0;
-	// 		let t1 = t0 + t_exp;
-	// 		let id = format!("{}/{}/{}",dataset_id,igra,iscan);
-	// 		let fp = Footprint{
-	// 		    orbit,
-	// 		    id:id.to_string(),
-	// 		    platform:platform.to_string(),
-	// 		    instrument:instrument.to_string(),
-	// 		    time_interval:(t0,t1),
-	// 		    outline
-	// 		};
-	// 		footprints.push(fp);
-	// 		iscan += mscan;
-	// 	    }
-	// 	}
-	// 	info!("Number of scan lines that have been split due to crossing the meridian boundary: {}",ncross);
-	// }
+	info!("Number of scan lines that have been split due to crossing the meridian boundary: {}",ncross);
     }
     let fps = Footprints{ footprints };
     fps.save_to_file(out_fn)?;
