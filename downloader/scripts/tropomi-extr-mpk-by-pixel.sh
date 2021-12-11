8c67c584f0d3dff19cb7e4d7c3d6d3cc3b4de01d
Berke DURAK <bd@exhrd.fr>
2021-12-11 11:47:30 +0000

    Update TROPOMI per-pixel extraction script.

--- downloader/scripts/tropomi-extr-mpk-by-pixel.sh
+++ downloader/scripts/tropomi-extr-mpk-by-pixel.sh
@@ -38,24 +38,35 @@ trace() {
 }
 
 main() {
-    for nc in $* ; do
-	echo $nc
-	id=${nc:t:r}
-	local work=$WORK_DIR/
-	local tmp_mpk=$work/$id.mpk
-	local tmp_mpk_log=$work/$id.mpk.log
-	local out=$OUT_DIR/tropomi/mpk-by-pixel
-	if [ -e $out/$id.mpk ]; then
-	    trace "Skipping already processed $nat"
-	else
-	    if ! $TROPOMIFPEX --by-pixel --output $tmp_mpk $nc >$tmp_mpk_log 2>&1 ; then
-		error "Could not extract MPK from NC for $id, RC $?"
-		continue
+    # for nc in $* ; do
+    # 	echo $nc
+    # done
+    while read -A a ; do
+	orbit=$a[1]
+	sel=( $a[2,$#a] )
+	echo "Orbit: $orbit selection: $sel"
+	for nc in $TROPOMI_SAVE/S5P_???????????????????????????????????????????????_${orbit}_?????????????????????????.nc ; do
+	    echo "Found $nc"
+	    id=${nc:t:r}
+	    local work=$WORK_DIR/
+	    local tmp_mpk=$work/$id.mpk
+	    local tmp_mpk_log=$work/$id.mpk.log
+	    local out=$OUT_DIR/tropomi/mpk-by-pixel
+	    if [ -e $out/$id.mpk ]; then
+		trace "Skipping already processed $nat"
+	    else
+		if ! $TROPOMIFPEX --by-pixel \
+		     --output $tmp_mpk \
+		     --selection $sel \
+		     -- $nc >$tmp_mpk_log 2>&1 ; then
+		    error "Could not extract MPK from NC for $id, RC $?"
+		    continue
+		fi
+		mv $tmp_mpk $out/$id.mpk
+		rm -f $tmp_nc_log
 	    fi
-	    mv $tmp_mpk $out/$id.mpk
-	    rm -f $tmp_nc_log
-	fi
+	done
     done
 }
 
-main $*
+main
