ae5ad66225a4eec96b572e706042631879d1e4ac
Berke DURAK <bd@exhrd.fr>
2022-04-22 17:32:45 +0000

    Also include TROPOMI and prior values, and produce an HDF5 output file giving the values.

--- downloader/src/mkretmap.rs
+++ downloader/src/mkretmap.rs
@@ -20,7 +20,11 @@ use serde_json::{Map,to_value};
 struct FancyFootprint {
     fp:Footprint,
     xch4:f64,
-    sigma_xch4:f64
+    sigma_xch4:f64,
+    xch4_prior:f64,
+    sigma_xch4_prior:f64,
+    xch4_tropomi:f64,
+    sigma_xch4_tropomi:f64
 }
 
 impl FootprintLike for FancyFootprint {
@@ -34,6 +38,10 @@ impl FootprintLike for FancyFootprint {
 	let mut map = Map::new();
 	map.insert(String::from("xch4"),to_value(self.xch4).unwrap());
 	map.insert(String::from("sigma_xch4"),to_value(self.sigma_xch4).unwrap());
+	map.insert(String::from("xch4_prior"),to_value(self.xch4_prior).unwrap());
+	map.insert(String::from("sigma_xch4_prior"),to_value(self.sigma_xch4_prior).unwrap());
+	map.insert(String::from("xch4_tropomi"),to_value(self.xch4_tropomi).unwrap());
+	map.insert(String::from("sigma_xch4_tropomi"),to_value(self.sigma_xch4_tropomi).unwrap());
 	map
     }
 }
@@ -46,16 +54,19 @@ fn main()->Result<(),Box<dyn Error>> {
 	.arg(Arg::with_name("out").short("o").long("output").value_name("PATH").takes_value(true).required(true))
 	.arg(Arg::with_name("input").short("i").long("input").value_name("PATH").takes_value(true).required(true))
 	.arg(Arg::with_name("iasifpdir").long("iasifpdir").value_name("PATH").takes_value(true).required(true))
+	.arg(Arg::with_name("limit").long("limit").value_name("N").takes_value(true).required(false))
 	.get_matches();
 
-    let out_fn = args.value_of("out").expect("Specify path to output file");
+    let out_base = args.value_of("out").expect("Specify output base");
     let input_dir = args.value_of("input").expect("Specify input directory");
     let iasi_fp_dir = args.value_of("iasifpdir").expect("Specify IASI footprint directory");
+    let limit = args.value_of("limit").map(|x| x.parse::<usize>().expect("Invalid count"));
     
     let iasi_ret_re = regex::Regex::new(r"^(IASI_xxx_.*)-(\d+)-(\d+)-(\d+)-ret.h5$")?;
 
     struct Retrieval {
 	path:OsString,
+	prefix:String,
 	base:String,
 	igra:usize,
 	iscan:usize,
@@ -67,35 +78,37 @@ fn main()->Result<(),Box<dyn Error>> {
 
     let mut ids : BTreeSet<String> = BTreeSet::new();
 
-    'outer: for dent in std::fs::read_dir(input_dir)?.flatten() {
+    let mut count = 0;
+    for dent in std::fs::read_dir(input_dir)?.flatten() {
+	if let Some(l) = limit {
+	    if count > l {
+		break;
+	    }
+	}
 	if dent.file_type()?.is_file() {
 	    let path = dent.path();
 	    if let Some(name) = path.file_name() {
 		if let Some(u) = name.to_str() {
 		    if let Some(cap) = iasi_ret_re.captures(u) {
 			let base = MiscError::from_option(cap.get(1),"Bad base name")?.as_str();
-			let igra =
-			    MiscError::from_option(cap.get(2),"Bad granule")?
-			    .as_str()
-			    .parse::<usize>()?;
-			let iscan =
-			    MiscError::from_option(cap.get(3),"Bad scan")?
-			    .as_str()
-			    .parse::<usize>()?;
-			let ipix =
-			    MiscError::from_option(cap.get(4),"Bad pixel")?
-			    .as_str()
-			    .parse::<usize>()?;
+			let igra_str = MiscError::from_option(cap.get(2),"Bad granule")?.as_str();
+			let igra = igra_str.parse::<usize>()?;
+			let iscan_str = MiscError::from_option(cap.get(3),"Bad scan")?.as_str();
+			let iscan = iscan_str.parse::<usize>()?;
+			let ipix_str = MiscError::from_option(cap.get(4),"Bad pixel")?.as_str();
+			let ipix = ipix_str.parse::<usize>()?;
 			let id = format!("{}/{}/{}/{}",base,igra,iscan,ipix);
+			let prefix = format!("{}-{}-{}-{}",base,igra_str,iscan_str,ipix_str);
 			ids.insert(id);
 			retrievals.push(Retrieval{
 			    path:path.clone().into_os_string(),
+			    prefix,
 			    base:base.to_string(),
 			    igra,
 			    iscan,
 			    ipix
 			});
-			break 'outer;
+			count += 1;
 		    }
 		}
 	    }
@@ -110,7 +123,6 @@ fn main()->Result<(),Box<dyn Error>> {
 
     // Load the footprints
     for name in mpk_names.iter() {
-    // let fp_from_path = |name:&String|->Result<(String,Footprints),Box<dyn Error>> {
 	let mut path : PathBuf = iasi_fp_dir.into();
 	path.push(name);
 	path.set_extension("mpk");
@@ -131,28 +143,60 @@ fn main()->Result<(),Box<dyn Error>> {
     }
 	
     // Process retrievals
+    let nret = retrievals.len();
     let mut footprints = Vec::new();
-    for Retrieval { path, base, igra, iscan, ipix } in retrievals.iter() {
-	info!("Opening {:?}",path);
+    let mut xch4_prior_arr = Array1::zeros(nret);
+    let mut sigma_xch4_prior_arr = Array1::zeros(nret);
+    let mut xch4_iasi_arr = Array1::zeros(nret);
+    let mut sigma_xch4_iasi_arr = Array1::zeros(nret);
+    let mut xch4_tropomi_arr = Array1::zeros(nret);
+    let mut sigma_xch4_tropomi_arr = Array1::zeros(nret);
+    for (iret,Retrieval{ path,prefix,base,igra,iscan,ipix }) in retrievals.iter().enumerate() {
+	info!("Opening retrieval file {:?}",path);
 	let fd = hdf5::File::open(path)?;
 
 	let xstar : Array1<f64> = fd.dataset("/res/xstar")?.read_1d()?;
 	let sxp : Array2<f64> = fd.dataset("/oi/sxp")?.read_2d()?;
+	let xa : Array1<f64> = fd.dataset("/prior/mean")?.read_1d()?;
+	let sx : Array2<f64> = fd.dataset("/prior/cov")?.read_2d()?;
 	let im_ch4 = fd.group("/im/ch4")?;
 	let ch4_i0 : usize = im_ch4.dataset("i0")?.read_scalar()?;
 	let ch4_i1 : usize = im_ch4.dataset("i1")?.read_scalar()?;
 	let operator_tc : Array1<f64> = im_ch4.dataset("units/ppmv_tc/operator")?.read_1d()?;
+
+	let sx_ch4 = sx.slice(s![ch4_i0..ch4_i1,ch4_i0..ch4_i1]);
 	let sxp_ch4 = sxp.slice(s![ch4_i0..ch4_i1,ch4_i0..ch4_i1]);
-	let sigma_xch4 = operator_tc.dot(&sxp_ch4.dot(&operator_tc));
+
+	let sigma_xch4_prior = operator_tc.dot(&sx_ch4.dot(&operator_tc)).sqrt();
+	let sigma_xch4 = operator_tc.dot(&sxp_ch4.dot(&operator_tc)).sqrt();
+
+	let xch4_prior = operator_tc.dot(&xa.slice(s![ch4_i0..ch4_i1]));
 	let xch4 = operator_tc.dot(&xstar.slice(s![ch4_i0..ch4_i1]));
-	// info!("XCH4 = {}",xch4);
+
+	let mut spc_path = PathBuf::from(input_dir);
+	spc_path.push(format!("{}-spc.h5",prefix));
+	info!("Opening spectrum file {:?}",spc_path);
+	let spc_fd = hdf5::File::open(spc_path)?;
+	let xch4_tropomi = spc_fd.dataset("/tropomi/xch4_corr")?.read_scalar()?;
+	let sigma_xch4_tropomi = spc_fd.dataset("/tropomi/xch4_sigma")?.read_scalar()?;
+
+	xch4_prior_arr[iret] = xch4_prior;
+	sigma_xch4_prior_arr[iret] = sigma_xch4_prior;
+	xch4_iasi_arr[iret] = xch4;
+	sigma_xch4_iasi_arr[iret] = sigma_xch4;
+	xch4_tropomi_arr[iret] = xch4_tropomi;
+	sigma_xch4_tropomi_arr[iret] = sigma_xch4_tropomi;
 
 	let id = format!("{}/{}/{}/{}",base,igra,iscan,ipix);
 	if let Some(fp) = index.get(&id) {
 	    let ffp = FancyFootprint {
 		fp:(*fp).clone(),
 		xch4,
-		sigma_xch4
+		sigma_xch4,
+		xch4_prior,
+		sigma_xch4_prior,
+		xch4_tropomi,
+		sigma_xch4_tropomi
 	    };
 	    footprints.push(ffp);
 	} else {
@@ -160,8 +204,26 @@ fn main()->Result<(),Box<dyn Error>> {
 	}
     }
 
-    info!("Exporting GeoJSON to {:?}",out_fn);
-    footprint::export_geojson(&footprints,&out_fn)?;
+    let geojson_fn = format!("{}-footprints.geojson",out_base);
+    info!("Exporting GeoJSON to {:?}",geojson_fn);
+    footprint::export_geojson(&footprints,&geojson_fn)?;
+
+    let cmp_fn = format!("{}-cmp.h5",out_base);
+    info!("Exporting comparison data to {:?}",cmp_fn);
+    let cmp_fd = hdf5::File::create(cmp_fn)?;
+    //cmp_fd.new_dataset_builder().with_data(&xch4_iasi_arr).create("xch4_iasi")?;
+
+    let wr = |name:&str,arr:&Array1<f64>|->Result<(),Box<dyn Error>> {
+	cmp_fd.new_dataset::<f64>().create(name,arr.dim())?
+	    .write(arr.view())?;
+	Ok(())
+    };
+    wr("xch4_prior",&xch4_prior_arr)?;
+    wr("xch4_iasi",&xch4_iasi_arr)?;
+    wr("xch4_tropomi",&xch4_tropomi_arr)?;
+    wr("sigma_xch4_prior",&sigma_xch4_prior_arr)?;
+    wr("sigma_xch4_iasi",&sigma_xch4_iasi_arr)?;
+    wr("sigma_xch4_tropomi",&sigma_xch4_tropomi_arr)?;
 
     Ok(())
 }
